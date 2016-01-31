27394e80844f2f6e3e5ad78ef11beb7674079278
ZUNbado <git@acs.li>
2016-01-31 13:54:49 +0100

    users model to new base model

--- web-app/app/users/controllers.py
+++ web-app/app/users/controllers.py
@@ -1,40 +1,15 @@
 from app.common.sql import getdb
+from app.common.model import Model
 from flask.ext.login import UserMixin
 
-class Users(object):
+class Users(Model):
+    table = 'users'
+
     def __init__(self):
         self.db = getdb()
 
-    def save(self, id = None, name = None, email = None, password = None):
-        if id:
-            return self.update(id, name, email, password)
-        else:
-            return self.add(name, email, password)
-
-    def update(self, id, name, email, password):
-        if self.db.update('users', dict(name = name, email = email, password = password), [ 'id = %s' % id ]):
-            self.db.commit()
-            return id
-        return False
-
-    def add(self, name, email, password):
-        if self.db.insert('users', dict( name = name, email = email, password = password)):
-            self.db.commit()
-            return self.db.cur.lastrowid
-        return False
-
-    def get(self, id):
-        check = self.db.getOne('users', '*', ( 'id = %s', id ) )
-        if check:
-            return check
-        return False
-
-    def getAll(self):
-        users = self.db.getAll('users')
-        return users if users else []
-
     def login(self, email, password):
-        user = self.db.getOne('users', '*', ( 'email = %s AND password = %s', [ email, password ]))
+        user = self.db.getOne(self.table, '*', ( 'email = %s AND password = %s', [ email, password ]))
         if user:
             return user
         return False
