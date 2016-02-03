33785b55dc7805215c7d7ae1e54fc639d479b481
ZUNbado <git@acs.li>
2016-02-03 19:41:37 +0100

    contacts limit to owner user

--- web-app/app/contacts/controllers.py
+++ web-app/app/contacts/controllers.py
@@ -1,9 +1,11 @@
 from app.common.sql import getdb
 from app.common.model import UserModel
+from flask.ext.login import current_user
 
 class Contacts(UserModel):
     table = 'contacts'
 
     def getAll(self):
-        items = self.db.leftJoin((self.table, 'contact_type'), ('*', [ 'name AS contact_type' ]), ('type', 'id'))
+        where = None if current_user.is_admin else [ '%s = %s' % ( self.usercol, current_user.get_id() ) ]
+        items = self.db.leftJoin((self.table, 'contact_type'), ('*', [ 'name AS contact_type' ]), ('type', 'id'), where)
         return items if items else []
